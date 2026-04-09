c9741efc6b905f6256f60268c1e5ea15b4abba1c
jbermudez0816 <jbermudez@iermb.edu.co>
2026-04-09 12:00:33 -0500

    server.py

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, Request, Header
 import cv2
 import numpy as np
-from pyzbar.pyzbar import decode
 import random
 
 app = FastAPI()
@@ -10,7 +9,7 @@ API_KEY = "ecoia123"
 
 @app.get("/")
 def home():
-    return {"msg": "Servidor EcoIA en la nube activo 🔥"}
+    return {"msg": "Servidor EcoIA estable con OpenCV 🔥"}
 
 @app.post("/upload")
 async def upload(request: Request, x_api_key: str = Header(None)):
@@ -28,22 +27,19 @@ async def upload(request: Request, x_api_key: str = Header(None)):
     # ===== PROCESAMIENTO =====
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     gray = cv2.equalizeHist(gray)
-    gray = cv2.resize(gray, None, fx=2, fy=2)
 
-    # ===== QR =====
+    # ===== QR CON OPENCV =====
+    detector = cv2.QRCodeDetector()
+    data, bbox, _ = detector.detectAndDecode(gray)
+
     id = "NA"
     nombre = "NA"
     grado = "NA"
 
-    qr_data = decode(gray)
-    
-
-    if qr_data:
-        for qr in qr_data:
-            text = qr.data.decode("utf-8")
-            parts = text.split("|")
-            if len(parts) == 3:
-                id, nombre, grado = parts
+    if data:
+        parts = data.split("|")
+        if len(parts) == 3:
+            id, nombre, grado = parts
 
     # ===== IA SIMULADA =====
     objeto = random.choice(["plastico", "carton", "vidrio", "lata"])
